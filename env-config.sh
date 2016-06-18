8cf1a9f3aa4052c21de53d6b985caba365a6b83a
khai96_ <hvksmr1996@gmail.com>
2016-06-18 20:08:40 +0700

    a shell will no longer appears in windows

--- env-config.sh
+++ env-config.sh
@@ -1,2 +1,2 @@
 
-echo `pkg-config --cflags gtk+-3.0` -o ./main.exe ./main.cpp `pkg-config --libs gtk+-3.0`
+echo `./platform-config.js` `pkg-config --cflags gtk+-3.0` -o ./main.exe ./main.cpp `pkg-config --libs gtk+-3.0`
