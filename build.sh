2b5711e9016ed010c9af2b1b7995468c99d8e692
khai96_ <hvksmr1996@gmail.com>
2016-06-18 20:47:40 +0700

    improve scripts

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 
-g++ -std=c++14 $(./env-config.sh) $(./debug-config.sh)
+g++ -std=c++14 $(./env-config.sh) $(./debug-config.sh) && echo "SUCCEED" || (echo "FAILED" 1>&2; exit 1)
--- test.sh
+++ test.sh
@@ -1,2 +1,2 @@
 
-./build.sh && ./run.sh && echo "Succeed!!" || echo "Failed" 1>&2
+./build.sh && ./run.sh
