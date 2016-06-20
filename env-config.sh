d3cb23c5a584c1b733d2ef062c0fe20886fdfe40
khai96_ <hvksmr1996@gmail.com>
2016-06-20 19:53:19 +0700

    change layout

--- env-config.sh
+++ env-config.sh
@@ -1,2 +1,5 @@
 
-echo $(./platform-config.js) $(pkg-config --cflags gtk+-3.0) -o ./main.exe ./main.cpp $(pkg-config --libs gtk+-3.0)
+echo $(./platform-config.js) \
+    $(pkg-config --cflags gtk+-3.0) \
+    -o ./main.exe ./main.cpp \
+    $(pkg-config --libs gtk+-3.0)
