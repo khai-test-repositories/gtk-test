32498465471e94742aaae20b4ec3f5fec5093aa1
khai96_ <hvksmr1996@gmail.com>
2016-09-04 15:21:26 +0700

    improve env-config

--- env-config.sh
+++ env-config.sh
@@ -1,5 +1,4 @@
 
 echo $(./platform-config.js) \
-    $(pkg-config --cflags gtk+-3.0) \
     -o ./.out/main.exe ./main.cpp -O3 \
-    $(pkg-config --libs gtk+-3.0)
+    $(pkg-config --cflags --libs gtk+-3.0)
